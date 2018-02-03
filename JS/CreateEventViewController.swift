5028d0c0665891bcb0a98ab47909179ba0792abb
郭芷瑄 <francescaeatonstuff@gmail.com>
2018-02-03 15:49:25 +0800

    第二次

--- JS/CreateEventViewController.swift
+++ JS/CreateEventViewController.swift
@@ -13,7 +13,6 @@ class CreateEventViewController: UIViewController, UIPickerViewDelegate, UIPicke
     @IBOutlet weak var dateField: UITextField!
     @IBOutlet weak var cityField: UITextField!
     
-    
     let picker = UIDatePicker()
     let cities = ["中正區","大同區","中山區", "松山區", "大安區", "萬華區", "信義區", "士林區", "北投區", "內湖區", "南港區", "文山區"]
     

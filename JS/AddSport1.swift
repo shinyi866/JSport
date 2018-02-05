7c6adca7bec1afda3b7bd1e188186a89e053ce6d
apple <apple@applede-macbook-pro-2.local>
2018-02-05 18:13:48 +0800

    UICollectionView

--- JS/AddSport1.swift
+++ JS/AddSport1.swift
@@ -8,7 +8,28 @@
 
 import UIKit
 
-class AddSport1: UIViewController {
+class AddSport1: UIViewController , UICollectionViewDataSource , UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet var LGCell: UICollectionView!
+    
+    var data:[String] = ["1","2","3","4","5","6","7","8","9"]
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GLCell", for: indexPath) as UICollectionViewCell
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cellToDeselect:UICollectionViewCell = LGCell.cellForItem(at: indexPath)!
+        cellToDeselect.contentView.backgroundColor = UIColor.blue
+        print("select item \(indexPath.row)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,15 +41,5 @@ class AddSport1: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
